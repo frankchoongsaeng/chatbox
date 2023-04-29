24c289103dedc16db481a8f0c6993b6872346077
Frank Choongsaeng <frankchoongsaeng@gmail.com>
2023-04-29 11:19:39 +0000

    added feature to show which users are typing

--- components/message-box.jsx
+++ components/message-box.jsx
@@ -1,69 +1,184 @@
-import { useState, useRef } from 'react';
-import { Picker } from 'emoji-mart';
-import 'emoji-mart/css/emoji-mart.css';
-import css from 'styles/chat.module.css';
+import { useState, useRef, useEffect, useCallback } from "react";
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
+import css from "styles/chat.module.css";
 
-function MessageBox({ onSend }) {
-	const [message, setMessage] = useState('');
-	const [showEmojis, setShowEmojis] = useState(false);
-	const messageBoxRef = useRef();
+function MessageBox({ onSend, onTypingActivityChanged, usersTyping = [] }) {
+  const [message, setMessage] = useActiveState("", onTypingActivityChanged);
+  const [showEmojis, setShowEmojis] = useState(false);
+  const messageBoxRef = useRef();
 
-	const sendMessage = () => {
-		if (!message) return;
-		onSend(message);
-		setMessage('');
-	};
+  const sendMessage = () => {
+    if (message.trim() === "") return;
+    onSend(message);
+    setMessage("");
+  };
 
-	const checkKeys = e => {
-		// make sure the emoji
+  const handleOnTextInputChange = (eventObject) => {
+    const currentValue = eventObject.target.value;
+    setMessage(currentValue);
+  };
 
-		if (!message) return;
-		if (e.key === 'Enter') {
-			sendMessage();
-		}
-	};
+  const checkKeys = (e) => {
+    // make sure the emoji
 
-	const addEmoji = emoji => setMessage(message => `${message} ${emoji.native}`);
+    if (message.trim() === "") return;
+    if (e.key === "Enter") {
+      sendMessage();
+    }
+  };
 
-	return (
-		<div className={css.chat_message_area}>
-			{showEmojis && (
-				<div className={`${css.picker_drawer}`}>
-					<Picker
-						set='apple'
-						title=''
-						emoji=''
-						onSelect={addEmoji}
-						showPreview={false}
-						style={{ width: '100%' }}
-					/>
-				</div>
-			)}
-			<button onClick={() => setShowEmojis(canShow => !canShow)} className={css.picker}>
-				😃
-			</button>
-			<input
-				className={css.message_box}
-				type='text'
-				value={message}
-				onChange={e => setMessage(e.target.value)}
-				onKeyUp={checkKeys}
-				placeholder='Start typing your message...'
-				autoFocus
-				ref={messageBoxRef}
-			/>
-			<button className={css.send_button} onClick={sendMessage}>
-				<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-					<path
-						strokeLinecap='round'
-						strokeLinejoin='round'
-						strokeWidth={2}
-						d='M12 19l9 2-9-18-9 18 9-2zm0 0v-8'
-					/>
-				</svg>
-			</button>
-		</div>
-	);
+  const addEmoji = (emoji) =>
+    setMessage((message) => `${message} ${emoji.native}`);
+
+  return (
+    <>
+      {usersTyping.length > 0 && (
+        <span className={css.is_typing_wrapper}>
+          {createTypingMessage(usersTyping)}
+        </span>
+      )}
+      <div className={css.chat_message_area}>
+        {showEmojis && (
+          <div className={`${css.picker_drawer}`}>
+            <Picker
+              set="apple"
+              title=""
+              emoji=""
+              onSelect={addEmoji}
+              showPreview={false}
+              style={{ width: "100%" }}
+            />
+          </div>
+        )}
+        <button
+          onClick={() => setShowEmojis((canShow) => !canShow)}
+          className={css.picker}
+        >
+          😃
+        </button>
+        <input
+          className={css.message_box}
+          type="text"
+          value={message}
+          onChange={handleOnTextInputChange}
+          onKeyUp={checkKeys}
+          placeholder="Start typing your message..."
+          autoFocus
+          ref={messageBoxRef}
+        />
+        <button className={css.send_button} onClick={sendMessage}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+            />
+          </svg>
+        </button>
+      </div>
+    </>
+  );
+}
+
+/**
+ * A function that observes a state and calls a provided function
+ * after a certain period if the state keeps changing within that period.
+ * Returns a `state` and a `setState`
+ * @param {any} initialValue state
+ * @param {function} cb callback handler to pass the current activity when the activity changes.
+ */
+function useActiveState(initialValue, cb = undefined) {
+  const isInitialLoadRef = useRef(true);
+  const [state, setState] = useState(initialValue);
+  const timeOfLastestActivityRef = useRef(Date.now());
+  const [isActive, setIsActive] = useState(false);
+  const observeActivityTimeoutIdRef = useRef(null);
+  const waitForIdleTimeoutRef = useRef(null);
+
+  const waitForIdle = useCallback(() => {
+    // reset the timeout if an activity occurred within the last 1 sec
+    if (waitForIdleTimeoutRef.current)
+      clearTimeout(waitForIdleTimeoutRef.current);
+    const maximumAllowedInactivityPeriod = 1000; // 1 second
+
+    // it's in an idle state if no activity
+    // has occurred for the last 3 seconds
+    waitForIdleTimeoutRef.current = setTimeout(() => {
+      setIsActive(false);
+      waitForIdleTimeoutRef.current = null;
+    }, maximumAllowedInactivityPeriod);
+  }, []);
+
+  /**
+   * change active state to true after 1 second
+   * if the last activity time is less than 1 second
+   * and the isActive is false
+   */
+  const waitForConstantActivity = useCallback(() => {
+    const minimalRequiredActivityPeriod = 1000; // 1 sec
+    const isObservingActivity = observeActivityTimeoutIdRef.current !== null;
+
+    if (!isObservingActivity) {
+      const timeoutId = setTimeout(() => {
+        const timeElapsedSinceLatestActivty =
+          Date.now() - timeOfLastestActivityRef.current;
+
+        if (
+          timeElapsedSinceLatestActivty < minimalRequiredActivityPeriod &&
+          !isActive
+        ) {
+          setIsActive(true);
+        }
+
+        observeActivityTimeoutIdRef.current = null;
+      }, minimalRequiredActivityPeriod);
+      observeActivityTimeoutIdRef.current = timeoutId;
+    }
+  }, [isActive]);
+
+  const observeActivity = () => {
+    // record time of last activity
+    timeOfLastestActivityRef.current = Date.now();
+    if (isActive) waitForIdle();
+    else waitForConstantActivity();
+  };
+
+  // invoke the callback whenever the activity state changes
+  useEffect(() => {
+    if (cb) cb(isActive);
+
+    // ensure to wait for idle state if current state is active
+    if(isActive) waitForIdle()
+  }, [isActive, cb]);
+
+  // run whenever the user state changes
+  useEffect(() => {
+    if (!isInitialLoadRef.current) {
+      observeActivity();
+    }
+  }, [state]);
+
+  useEffect(() => {
+    isInitialLoadRef.current = false;
+  }, []);
+
+  return [state, setState, isActive];
+}
+
+function createTypingMessage(listOfTypingUsers) {
+  if (listOfTypingUsers.length === 0) return "";
+  else if (listOfTypingUsers.length === 1)
+    return `${listOfTypingUsers[0]} is typing...`;
+  else if (listOfTypingUsers.length === 2)
+    return `${listOfTypingUsers[0]} & ${listOfTypingUsers[1]} are typing...`;
+  else return "multiple users are typing...";
 }
 
 export default MessageBox;
--- pages/chat.jsx
+++ pages/chat.jsx
@@ -1,130 +1,159 @@
-import { useEffect, useRef } from 'react';
-import { useDispatch } from 'react-redux';
-import { recieve, loadHistory } from 'store/features/chat';
-import _ from 'lodash';
-import io from 'socket.io-client';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import Container from 'components/container';
-import MessageBox from 'components/message-box';
+import { useEffect, useState, useCallback, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { recieve, loadHistory } from "store/features/chat";
+import _ from "lodash";
+import io from "socket.io-client";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Container from "components/container";
+import MessageBox from "components/message-box";
 
-import ChatBody from 'components/chat-body';
-import { logout } from 'store/features/user';
-import { useCookies } from 'react-cookie';
+import ChatBody from "components/chat-body";
+import { logout } from "store/features/user";
+import { useCookies } from "react-cookie";
 
-import css from 'styles/chat.module.css';
+import css from "styles/chat.module.css";
 
 export default function Chat({ username }) {
-	const dispatch = useDispatch();
-	const ioRef = useRef();
-	const [usernameCookie, , removeCookie] = useCookies(['username']);
-	const router = useRouter();
+  const dispatch = useDispatch();
+  const ioRef = useRef();
+  const [usernameCookie, , removeCookie] = useCookies(["username"]);
+  const router = useRouter();
+  const [usersTyping, setsUsersTyping] = useState([]);
 
-	const sendMessage = message => {
-		ioRef.current.emit('message', { message, user: username });
-	};
+  const sendMessage = (message) => {
+    ioRef.current.emit("message", { message, user: username });
+  };
 
-	const leaveChat = () => {
-		ioRef.current.disconnect();
-		removeCookie('username');
-		router.replace('/join');
-	};
+  const sendTypingActivity = useCallback(
+    (isTyping) => {
+      if (!ioRef.current) return;
+      if (isTyping) ioRef.current.emit("typing", username);
+      else ioRef.current.emit("typing-stopped", username);
+    },
+    [username]
+  );
 
-	const attachSocketListeners = useRef(() => {
-		ioRef.current.emit('user-joined', { user: username }, previousMessages => {
-			dispatch(loadHistory(previousMessages));
-		});
+  const leaveChat = () => {
+    ioRef.current.disconnect();
+    removeCookie("username");
+    router.replace("/join");
+  };
 
-		ioRef.current.on('joined', message => {
-			dispatch(recieve(message));
-		});
+  useEffect(() => {
+    if (!usernameCookie.username) {
+      router.push("/join");
+    }
+  }, [usernameCookie, router]);
 
-		ioRef.current.on('message', message => {
-			dispatch(recieve(message));
-		});
+  // Attach listeners
+  useEffect(() => {
+    const ioInstance = io();
 
-		// return () => {
-		// 	ioRef.current.emit('leave', { user: username });
-		// 	ioRef.current.disconnect();
-		// };
-	});
+    ioInstance.emit("user-joined", { user: username }, (previousMessages) => {
+      dispatch(loadHistory(previousMessages));
+    });
 
-	useEffect(() => {
-		console.log(usernameCookie);
-		if (!usernameCookie.username) {
-			router.push('/join');
-		}
-	}, [usernameCookie, router]);
+    ioInstance.on("joined", (message) => {
+      dispatch(recieve(message));
+    });
 
-	useEffect(() => {
-		ioRef.current = io();
-		attachSocketListeners.current();
-	}, [username]);
+    ioInstance.on("message", (message) => {
+      dispatch(recieve(message));
+    });
 
-	return (
-		<>
-			<Head>
-				<title>Chatbox</title>
-				<meta name='description' content='a general chat room for tech gods' />
-				<meta
-					name='viewport'
-					content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
-				/>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
+    ioInstance.on("typing", (username) => {
+      // using Sets ensure we don't get a duplication of usernames
+      setsUsersTyping((previouslyTypingList) => {
+        const newSet = new Set([...previouslyTypingList, username]);
+        return [...newSet];
+      });
+    });
 
-			<Container>
-				<div className={css.flex_layout}>
-					<div className={css.chat_header}>
-						<div></div>
-						<h4>GENERAL GROUP</h4>
-						<button className='regular' onClick={leaveChat}>
-							Leave
-						</button>
-					</div>
-					<ChatBody username={username} />
-					<MessageBox onSend={sendMessage} />
-				</div>
-			</Container>
-		</>
-	);
+    ioInstance.on("typing-stopped", (username) => {
+      // using sets to reduce time complexity of removing users that stopped typing
+      setsUsersTyping((previouslyTypingList) => {
+        const newSet = new Set(previouslyTypingList);
+        newSet.delete(username);
+        return [...newSet];
+      });
+    });
+
+    ioRef.current = ioInstance;
+
+    return () => {
+      ioInstance.disconnect();
+    };
+  }, [username, dispatch]);
+
+  return (
+    <>
+      <Head>
+        <title>Chatbox</title>
+        <meta name="description" content="a general chat room for tech gods" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Container>
+        <div className={css.flex_layout}>
+          <div className={css.chat_header}>
+            <div></div>
+            <h4>GENERAL GROUP</h4>
+            <button className="regular" onClick={leaveChat}>
+              Leave
+            </button>
+          </div>
+          <ChatBody username={username} />
+          <MessageBox
+            onSend={sendMessage}
+            onTypingActivityChanged={sendTypingActivity}
+            usersTyping={usersTyping}
+          />
+        </div>
+      </Container>
+    </>
+  );
 }
 
 // utility function to parse cookies
-const parseCookies = req => {
-	if (!req.headers.cookie) return {};
-	let cookies = req.headers.cookie.split(';').map(c_str => {
-		let pair = c_str.trim().split('=');
-		return { [pair[0]]: pair[1] };
-	});
-	return cookies.reduce((prev, curr) => ({ ...prev, ...curr }), {});
+const parseCookies = (req) => {
+  if (!req.headers.cookie) return {};
+  let cookies = req.headers.cookie.split(";").map((c_str) => {
+    let pair = c_str.trim().split("=");
+    return { [pair[0]]: pair[1] };
+  });
+  return cookies.reduce((prev, curr) => ({ ...prev, ...curr }), {});
 };
 
 //TODO implement reroute back to home page when cookie not set
 export const getServerSideProps = async ({ req, query }) => {
-	// redirect back to the join page if cookie was not set
-	const cookies = parseCookies(req);
-	if (!cookies.username) {
-		return {
-			redirect: {
-				destination: `/join`,
-				permanent: false,
-			},
-		};
-	}
+  // redirect back to the join page if cookie was not set
+  const cookies = parseCookies(req);
+  if (!cookies.username) {
+    return {
+      redirect: {
+        destination: `/join`,
+        permanent: false,
+      },
+    };
+  }
 
-	// if cookie is set, but query is not, build query
-	if (_.isEmpty(query) || !query.username) {
-		return {
-			redirect: {
-				destination: `/chat?username=${query.username}`,
-				permanent: false,
-			},
-		};
-	}
+  // if cookie is set, but query is not, build query
+  if (_.isEmpty(query) || !query.username) {
+    return {
+      redirect: {
+        destination: `/chat?username=${query.username}`,
+        permanent: false,
+      },
+    };
+  }
 
-	// otherwise, return the username to the component
-	return {
-		props: { username: query.username },
-	};
+  // otherwise, return the username to the component
+  return {
+    props: { username: query.username },
+  };
 };
